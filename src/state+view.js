37b3097cc4ba90153f742540c4021895e0bf9aed
Ivan <antipchel@gmail.com>
2021-05-13 20:55:07 +0300

    trying proxy

--- src/state+view.js
+++ src/state+view.js
@@ -18,7 +18,7 @@ const saySuccess = (text, feedback, input) => {
   input.classList.remove('is-invalid');
 };
 
-const downloadRSS = (link) => axios(`https://hexlet-allorigins.herokuapp.com/raw?disableCache=true&url=${encodeURIComponent(link)}`,
+const downloadRSS = (link) => axios(`https://hexlet-allorigins.herokuapp.com/raw?url=${encodeURIComponent(link)}`,
   { responseType: 'text' })
   .then((resp) => parseRSS(link, resp.data));
 
